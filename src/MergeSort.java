cf5dc0ea6fa8bdc6b877523b527db079c66dfbec
mowang <3213744542@qq.com>
2024-05-26 15:30:11 +0800

    完善归并排序

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+
 import sun.reflect.generics.tree.ArrayTypeSignature;
 
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import java.util.Arrays;
  */
 public class MergeSort {
     public static void main(String[] args) {
-        int testTime=5000;
+        int testTime=1;
         int maxSize =100;
         boolean succeed =true;
 
@@ -18,9 +19,10 @@ public class MergeSort {
         for (int i = 0; i < testTime; i++) {
             int[] arr1 =UserDevice.generateRandomArray(maxSize,maxSize);
             int[] arr2 =UserDevice.copyArray(arr1);
-            mergeSort(arr1,0, arr1.length-1);
+            mergeSort(arr1,0, arr1.length - 1);
             Arrays.sort(arr2);
             if (!UserDevice.isEqual(arr1, arr2)) {
+                System.out.println("打印i =  " + i);
                 succeed = false;
                 UserDevice.printArray(arr1);
                 UserDevice.printArray(arr2);
@@ -32,33 +34,33 @@ public class MergeSort {
 
     }
 
-    public static void mergeSort(int[] arr,int l,int r){
-        if(r-l<2){
+    public static void mergeSort(int[] arr, int l, int r){
+        if(l == r){
             return ;
         }
-        int m = l+(r-l)/2;
-        mergeSort(arr,l,m);
-        mergeSort(arr,m,r);
-        merge(arr,l,m,r);
+        int m = l + ((r - l) >> 1);
+        mergeSort(arr, l, m);
+        mergeSort(arr,m+1 , r);
+        merge(arr, l, m, r);
 
     }
 
-    public static void merge(int[] arr,int l,int m,int r){
-        int[] help =new int[r-l+1];
-        int i =0;
-        int p1 =l;
-        int p2 =m +1 ;
-        while(p1<=m&&p2<=r){
-            help[i++]=arr[p1]<arr[p2]?arr[p1++]:arr[p2++];
+    public static void merge(int[] arr, int l, int m, int r){
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m+1;
+        while(p1 <= m && p2 <= r) {
+            help[i ++] = arr[p1] < arr[p2] ? arr[p1 ++] : arr[p2 ++];
         }
-        if(p1<=m){
-            help[i++]=arr[p1++];
+        while(p1 <= m) {
+            help[i ++] = arr[p1 ++];
         }
-        if(p2<=r){
-            help[i++]=arr[p2++];
+        while(p2 <= r) {
+            help[i ++] = arr[p2 ++];
         }
-        for (i= 0; i < arr.length ; i++) {
-            arr[i]=help[i];
+        for ( i = 0; i < help.length; i ++) {
+            arr[l + i] = help[i];
         }
     }
 }
